5f82ff9d8c0accbdabce4c81199afe4a41b4db46
whiteRa2bit <pavel.fakanov@gmail.com>
2019-11-07 10:13:38 +0300

    add base transformer scripts

--- sh/base_transformer/generate.sh
+++ sh/base_transformer/generate.sh
@@ -1,6 +1,6 @@
 FAIRSEQ_PATH=/home/phramer/fairseq
-DATA_PATH=/home/phramer/data/extra_small_ria/data-bin
-LM_CHECKPOINT_PATH=/home/whiteRa2bit/checkpoints/extra_small_ria
+DATA_PATH=/home/phramer/data/short_ria/data-bin
+LM_CHECKPOINT_PATH=/home/whiteRa2bit/checkpoints/short_ria
 
 python $FAIRSEQ_PATH/generate.py $DATA_PATH --path $LM_CHECKPOINT_PATH/checkpoint_best.pt \
      --beam 5 --remove-bpe --skip-invalid-size-inputs-valid-test
