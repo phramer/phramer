531f18caf642b23385bc5d2b2312131b4c1020dc
NickShatalov <nickshatalov@yandex.ru>
2019-11-11 15:31:37 +0300

    enh: add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+
+requirements = ['pandas==0.24.2',
+                'dvc==0.66.11',
+                'comet-ml==2.0.16',
+                'bs4==0.0.1',
+                'tqdm==4.37.0']
+
+dev_requirements = ['pre-commit',
+                    'black']
+
 setup(
     name='phramer',
     version='0.1',
@@ -12,5 +22,7 @@ setup(
             # 'fairseq-validate = fairseq_cli.validate:cli_main',
         ],
     },
+    install_requires=requirements,
+    extras_require={'dev': dev_requirements},
 )
 
