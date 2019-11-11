2e0f34f4d1f3a8ebd10027730baf9030cbf2aa60
NickShatalov <nickshatalov@yandex.ru>
2019-11-11 13:48:24 +0300

    enh: add phramer-preprocess command

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    entry_points={
+        'console_scripts': [
+            'phramer-preprocess = phramer.preprocess_text:main',
+            # 'fairseq-validate = fairseq_cli.validate:cli_main',
+        ],
+    },
 )
 
