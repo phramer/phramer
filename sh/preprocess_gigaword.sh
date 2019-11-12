535a40f52b02d27e4520ffc2cc8b24f393dfebd4
whiteRa2bit <pavel.fakanov@gmail.com>
2019-11-12 14:53:47 -0500

    add scripts for lm pipeline

--- sh/preprocess_gigaword.sh
+++ sh/preprocess_gigaword.sh
@@ -1,8 +1,8 @@
-DATA_PATH=/home/phramer/data/gigaword
-FAIRSEQ_PATH=/home/phramer/fairseq/
-DEST_DIR=/home/phramer/data/gigaword/data-bin/
+DATA_PATH=/data/pafakanov/gigaword/
+FAIRSEQ_PATH=/home/pafakanov/fairseq/
+DEST_DIR=/data/pafakanov/gigaword/data-bin/
 
 python3 $FAIRSEQ_PATH/preprocess.py --source-lang articles --target-lang summaries \
   --trainpref $DATA_PATH/train.gigaword --validpref $DATA_PATH/valid.gigaword --testpref $DATA_PATH/test.gigaword \
-  --destdir $DEST_DIR --workers 70 --bpe bert --dataset-impl lazy
+  --destdir $DEST_DIR --workers 70
 
