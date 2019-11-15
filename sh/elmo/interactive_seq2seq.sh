6560c29e6965dc1b9b2e5935082fd05e13d6f9d4
whiteRa2bit <pavel.fakanov@gmail.com>
2019-11-15 10:09:07 -0500

    modify files

--- sh/elmo/interactive_seq2seq.sh
+++ sh/elmo/interactive_seq2seq.sh
@@ -2,6 +2,8 @@ FAIRSEQ_PATH=/home/pafakanov/fairseq
 DATA_PATH=/data/pafakanov/short_ria/data-bin
 CHECKPOINT_PATH=/data/pafakanov/checkpoints/short_ria_seq2seq
 
+python3 preprocess_article.py --data_path data.txt --save_path data_new.txt \
+
 python $FAIRSEQ_PATH/interactive.py ${DATA_PATH} --path ${CHECKPOINT_PATH}/checkpoint_best.pt --remove-bpe --gen-subset test \
-	                   --min-len 1 --beam 5 --no-repeat-ngram 3 --nbest 1
+	                   --min-len 1 --beam 5 --no-repeat-ngram 3 --nbest 1 --input data_new.txt
 
--- sh/elmo/preprocess_article.py
+++ sh/elmo/preprocess_article.py
@@ -1,15 +1,26 @@
+import argparse
 from phramer.data.dataset import RIANewsDataset
 
 ria = RIANewsDataset()
 
 
-def process_article(article):
-    print(ria._process_article(article))
+def process_article(data_path, save_path):
+    with open(data_path, 'r+') as f:
+        article = f.read()
+
+    article = ria._process_article(article)
+    article = article.replace('\n', ' ')
+
+    f = open(save_path, 'w+')
+    f.write(article)
+    f.close()
 
 def main():
-    article = input()
-    process_article(article)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, required=True, help='path to data to process')
+    parser.add_argument("--save_path", type=str, required=True, help='path where to save after processing')
+    args = parser.parse_args()
+    process_article(args.data_path, args.save_path)
 
 if __name__ == "__main__":
     main()
