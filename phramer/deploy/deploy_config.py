92ba061c4f317f3d528813d3768b2c786ee18ef1
whiteRa2bit <pavel.fakanov@gmail.com>
2019-11-15 23:59:29 +0300

    add build_args

---
+++ phramer/deploy/deploy_config.py
@@ -0,0 +1,4 @@
+FAIRSEQ_PATH='/home/pavel_fakanov/fairseq'
+MODELS_CHECKPOINTS = [
+        
+]
---
+++ phramer/deploy/models_config/ria_seq2seq.py
@@ -0,0 +1,15 @@
+FAIRSEQ_PATH='/home/pavel_fakanov/fairseq'
+DATA_PATH='/home/pavel_fakanov/data/ria_seq2seq/data-bin/'
+CHECKPOINT_PATH='/home/pavel_fakanov/checkpoints/ria_seq2seq'
+DATASET_NAME='ria'
+INPUT_FILE_NAME='data.txt'
+REMOVE_BPE=True
+MIN_LEN=1
+BEAM=5
+NO_REPEAT_NGRAM=3
+NBEST=1
+BUFFER_SIZE=0
+CUDA_VISIBLE_DEVICES = [
+    1,
+]
+
