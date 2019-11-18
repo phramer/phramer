4f34c55e9fd2fe8eb6928634935c37729e913d03
whiteRa2bit <pavel.fakanov@gmail.com>
2019-11-18 05:23:35 -0500

    delete duplicates

--- sh/architectures/hugging_face/run_summarization_finetuning.sh
+++
@@ -1,9 +0,0 @@
-export DATA_PATH=/data/pafakanov/cnn_dailymail/
-export HUG_FACE_PATH=/home/pafakanov/transformers
-
-python $HUG_FACE_PATH/examples/run_summarization.py \
-	--output_dir output \
-	--per_gpu_train_batch_size 1\
-	--model_type bert2bert \
-	--do_train True\
-	--data_dir=$DATA_PATH \
--- sh/datasets/ria/preprocess_ria.sh
+++
@@ -1,8 +0,0 @@
-DATA_PATH=/data/pafakanov/short_ria
-FAIRSEQ_PATH=/home/pafakanov/fairseq
-DEST_DIR=/data/pafakanov/short_ria/data-bin
-
-python3 $FAIRSEQ_PATH/preprocess.py --source-lang articles --target-lang summaries \
-  --trainpref $DATA_PATH/train.ria --validpref $DATA_PATH/valid.ria --testpref $DATA_PATH/test.ria \
-  --destdir $DEST_DIR --workers 70
-
